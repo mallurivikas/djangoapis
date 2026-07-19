fc3f3adc086595589905611cf06f3ae7c51c5a3d
mallurivikas <vikasmalluri@gmail.com>
2026-07-19 13:09:41 +0530

    Implementing APIs

--- library/views.py
+++ library/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Book
-# Create your views here.
-class BookList(APIView):
+from .serializers import BookSerializer
+
+class BookView(APIView):
     def get(self,request):
         books=Book.objects.all()
         serializer=BookSerializer(books,many=True)
-        return Response(serializer.data)
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
+    def post(self,request):
+        serializer=BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
