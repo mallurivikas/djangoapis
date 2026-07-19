2115d597e85a6a0269f779231b4075d04111d65d
mallurivikas <vikasmalluri@gmail.com>
2026-07-19 16:52:58 +0530

    Implementing PUT PATCH DELETE APIs

--- library/urls.py
+++ library/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BookView
+from .views import BookView, BookDetail
 
 urlpatterns=[
-    path("books/",BookView.as_view())
+    path("books/",BookView.as_view()),
+    path("books/<int:book_id>/",BookDetail.as_view())
 ]
--- library/views.py
+++ library/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 from .models import Book
 from .serializers import BookSerializer
@@ -24,4 +25,64 @@ class BookView(APIView):
         return Response(
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
+        )
+
+class BookDetail(APIView):
+    def get(self,request,book_id):
+        book=get_object_or_404(
+            Book,
+            id=book_id
+        )
+        serializer=BookSerializer(book)
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
+    def put(self,request,book_id):
+        book=get_object_or_404(
+            Book,
+            id=book_id
+        )
+        serializer=BookSerializer(
+            book,
+            data=request.data
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    def patch(self,request,book_id):
+        book=get_object_or_404(
+            Book,
+            id=book_id
+        )
+        serializer=BookSerializer(
+            book,
+            data=request.data,
+            partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    def delete(self,request,book_id):
+        book=get_object_or_404(
+                Book,
+                id=book_id
+            )
+        book.delete()
+        return (
+            Response(status.HTTP_204_NO_CONTENT)
         )
