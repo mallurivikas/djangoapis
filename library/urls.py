1b9c042147ec61268566ae41b85281eb086ba46d
mallurivikas <vikasmalluri@gmail.com>
2026-07-22 22:10:42 +0530

    Implementing Login Endpoint

--- library/urls.py
+++ library/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BookView, BookDetail, RegistrationView
+from .views import BookView, BookDetail, RegistrationView,LoginView
 
 urlpatterns=[
     path("books/",BookView.as_view()),
     path("books/<int:book_id>/",BookDetail.as_view()),
     path("register/",RegistrationView.as_view()),
+    path("login/",LoginView.as_view()),
 ]
--- library/views.py
+++ library/views.py
@@ -1,7 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 from django.shortcuts import get_object_or_404
+from django.contrib.auth import authenticate
+
 
 from .models import Book
 from .serializers import BookSerializer
@@ -115,4 +118,29 @@ class RegistrationView(APIView):
         return Response(
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
+        )
+
+class LoginView(APIView):
+    def post(self,request):
+        username=request.data.get("username")
+        password=request.data.get("password")
+
+        user=authenticate(
+            username=username,
+            password=password
+        )
+        if user is None:
+            return Response(
+                {
+                    "error":"Invalid Credentials",
+                },
+                status=status.HTTP_401_UNAUTHORIZED 
+            )
+        refresh=RefreshToken.for_user(user)
+        return Response(
+            {
+                "refresh":str(refresh),
+                "access":str(refresh.access_token)
+            },
+            status=status.HTTP_200_OK
         )
